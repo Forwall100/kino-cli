7dfff1ff51c95f54cc0da381741354b2b9372e5e
IF <hezeri@gmail.com>
2024-05-06 01:52:27 +0700

    add browser parametr

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import requests
+import sys
 from bs4 import BeautifulSoup
-from inquirer import prompt, List 
+from inquirer import prompt, List
 import os
 import tempfile
 import subprocess
-
+import webbrowser
 
 def search_kinopoisk_ids(query):
     search_url = f'https://www.kinopoisk.ru/index.php?kp_query=%{query}'
@@ -15,69 +16,70 @@ def search_kinopoisk_ids(query):
     ids = [i.a['href'].split('/')[2] for i in results]
     return dict(zip(names, ids))
 
-
 def search_sources(id):
     source_url = f'https://kinobox.tv/api/players?kinopoisk={id}'
     r = requests.get(source_url)
     return r.json()
 
-
-def show_iframe(url):
+def show_iframe(url, use_browser=False):
     html_content = f"""
     <!DOCTYPE html>
     <html>
     <head>
-        <style>
-            body {{
-                margin: 0;
-                padding: 0;
-                background-color: #333; 
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                height: 100vh;
-            }}
-            .iframe-container {{
-                border-radius: 20px; 
-                overflow: hidden;
-                box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
-            }}
-            iframe {{
-                width: 80vw;
-                height: 80vh; 
-                border: none;
-            }}
-        </style>
+    <style>
+    body {{
+    margin: 0;
+    padding: 0;
+    background-color: #333;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    }}
+    .iframe-container {{
+    border-radius: 20px;
+    overflow: hidden;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
+    }}
+    iframe {{
+    width: 80vw;
+    height: 80vh;
+    border: none;
+    }}
+    </style>
     </head>
     <body>
-        <div class="iframe-container">
-            <iframe src="{url}" frameborder="0" allowfullscreen></iframe>
-        </div>
+    <div class="iframe-container">
+    <iframe src="{url}" frameborder="0" allowfullscreen></iframe>
+    </div>
     </body>
     </html>
     """
+
     with tempfile.NamedTemporaryFile(mode="w", delete=False, suffix=".html") as temp_file:
         temp_filename = temp_file.name
         temp_file.write(html_content)
 
-    subprocess.Popen(['electron', temp_filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    if use_browser:
+        webbrowser.open(f"file://{temp_filename}", new=2)
+    else:
+        subprocess.Popen(['electron', temp_filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+
     print("Enjoy!")
     exit()
 
-
-def main(query):
+def main(query, use_browser=False):
     os.system('clear')
 
     movie_ids = search_kinopoisk_ids(query)
     movie_names = list(movie_ids.keys())
-
     movie_choice = prompt([
         List('movie', 'Выберите фильм', choices=movie_names)
     ], raise_keyboard_interrupt=True)['movie']
-
+    
     movie_id = movie_ids[movie_choice]
-    sources = search_sources(movie_id)
 
+    sources = search_sources(movie_id)
     source_choices = [f"{source['source']} - {source['translations'][0]['name']}" for source in sources if source['iframeUrl']]
 
     os.system('clear')
@@ -87,13 +89,19 @@ def main(query):
     ], raise_keyboard_interrupt=True)['source']
 
     selected_source = next(source for source in sources if f"{source['source']} - {source['translations'][0]['name']}" == source_choice)
-    print(f"Выбранный iframeUrl: {selected_source['iframeUrl']}")
-    show_iframe(selected_source['iframeUrl'])
 
+    show_iframe(selected_source['iframeUrl'], use_browser)
 
 if __name__ == '__main__':
-    import sys
-    if len(sys.argv) < 2:
-        print("Использование: python kino-cli.py <название фильма>")
+    use_browser = '--browser' in sys.argv
+    if len(sys.argv) < 2 or (len(sys.argv) == 2 and '--browser' in sys.argv):
+
+        print("""
+Использование: python kino-cli.py <название фильма> 
+
+Параметры:
+--browser: открывать плеер в браузере
+              """)
     else:
-        main(sys.argv[1])
+        query = sys.argv[1] if not use_browser else sys.argv[2]
+        main(query, use_browser)
